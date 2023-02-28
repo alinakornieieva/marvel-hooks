00bf3e9385854aa2a8276169bebc6d3d0dbe3158
alinakornieieva <alinakorneeva401@gmail.com>
2023-02-28 12:56:45 +0300

    add lazy loading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
+import { lazy, Suspense } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import PageNotFound from './components/Pages/404 Page/404';
-import ComicPage from './components/Pages/ComicPage/ComicPage';
-import ComicsPage from './components/Pages/ComicsPage';
-import CharPage from './components/Pages/CharPage';
+import Preloader from './components/Preloader/Preloader';
 
 //динамические импорты
+const PageNotFound = lazy(() => import('./components/Pages/404 Page/404'))
+const CharPage = lazy(() => import('./components/Pages/CharPage'))
+const ComicPage = lazy(() => import('./components/Pages/ComicPage/ComicPage'))
+const ComicsPage = lazy(() => import('./components/Pages/ComicsPage'))
+
+
 
 const App = () => {
   return (
     <Router>
       <div className="App">
       <Header/>
+      <Suspense fallback={<Preloader/>}>
       <Routes>
         <Route path='*' element={<PageNotFound/>}/>
         <Route path='/' element={<CharPage/>}/>
         <Route path='/comics' element={<ComicsPage/>}/>
         <Route path='/comics/:comicId' element={<ComicPage/>}/>
       </Routes>
+      </Suspense>
     </div>
     </Router>
   );
