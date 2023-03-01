9aeceea488216f37287b5cbe047815e481f6bb14
alinakornieieva <alinakorneeva401@gmail.com>
2023-03-01 20:58:59 +0300

    create a char form using formik

--- my-app/src/components/FindChar/FindChar.js
+++ my-app/src/components/FindChar/FindChar.js
@@ -1,14 +1,24 @@
 import './FindChar.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup'
 
 const FindChar = () => {
     return(
         <div className="find-char">
             <div className='find-char-title'>Or find a character by name:</div>
-            <form className='form'>
-                <input className='input' type="text" name="character" id="character" placeholder="Enter name"/>
-                <button className="btn-1" type="submit">FIND</button>
-            </form>
-
+            <Formik
+            initialValues={{character: ''}}
+            validationSchema={Yup.object({
+                character: Yup.string().required('This field is required')
+            })}
+            onSubmit={(values)=> console.log(values)}
+            >
+                <Form className='form'>
+                    <Field className='input' type="text" name="character" id="character" placeholder="Enter name"/>
+                    <ErrorMessage name='character' component='div'/>
+                    <button className="btn-1" type="submit">FIND</button>
+                </Form>
+            </Formik>
         </div>
     )
 }
